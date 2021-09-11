57212d3ce76757d2a84b90d36fa79fbbbdcd3c16
Vieira Neto <vieiranetoc@gmail.com>
2021-09-11 19:08:51 -0300

    add test diagonal
    Signed-off-by: Vieira Neto <vieiranetoc@gmail.com>

--- Trabalho-1/testa_velha.cpp
+++ Trabalho-1/testa_velha.cpp
@@ -59,3 +59,11 @@ TEST_CASE("Testa velha", "[single-file]")
 						{0, 2, 1}};
 	REQUIRE(VerificaVelha(teste1) == 1);
 }
+
+TEST_CASE("Testa Velha diagonal")
+{
+	int teste1[3][3] = {{2, 0, 1},
+						{2, 2, 1},
+						{0, 2, 2}};
+	REQUIRE(VerificaVelha(teste1) == 2);
+}
