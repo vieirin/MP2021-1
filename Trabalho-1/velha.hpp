3b2db2a05f4d0887cccf45b717533f8cf4f5e483
Vieira Neto <vieiranetoc@gmail.com>
2021-09-11 18:49:15 -0300

    add direcao enum
    Signed-off-by: Vieira Neto <vieiranetoc@gmail.com>

--- Trabalho-1/velha.hpp
+++ Trabalho-1/velha.hpp
@@ -1,3 +1,10 @@
 int VerificaVelha(int velha[3][3]);
 int VerificaLinha(int velha[3]);
 int VerificaColuna(int velha[3][3], int col);
+int VerificaDiagonal(int velha[3][3]);
+
+enum Direcao
+{
+    direita,
+    esquerda
+}
