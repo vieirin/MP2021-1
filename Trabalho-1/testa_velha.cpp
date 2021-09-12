de7ee6368935aa9c6b08ba26063086e1255a7a3b
Vieira Neto <vieiranetoc@gmail.com>
2021-09-12 18:07:15 -0300

    rename test
    Signed-off-by: Vieira Neto <vieiranetoc@gmail.com>

--- Trabalho-1/testa_velha.cpp
+++ Trabalho-1/testa_velha.cpp
@@ -20,7 +20,7 @@ TEST_CASE("Testa O vencedor na linha")
 	REQUIRE(VerificaLinha(test) == 2);
 }
 
-TEST_CASE("Testa empate na linha")
+TEST_CASE("Testa indefinido na linha")
 {
 	int test[3] = {0, 2, 2};
 	REQUIRE(VerificaLinha(test) == -1);
@@ -73,6 +73,11 @@ TEST_CASE("Testa velha", "[single-file]")
 						{0, 1, 2},
 						{2, 1, 1}};
 	REQUIRE(VerificaVelha(teste4) == -1);
+
+	int teste[3][3] = {{1, 1, 1},
+					   {2, 1, 2},
+					   {2, 2, 1}};
+	REQUIRE(VerificaVelha(teste) == 1);
 
 	int teste5[3][3] = {{2, 2, 2},
 						{1, 1, 1},
